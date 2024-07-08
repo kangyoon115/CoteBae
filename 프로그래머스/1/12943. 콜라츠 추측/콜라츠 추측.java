e2fb92a67e42bd9f4dc67dbf8f6b950c5ad9e5e6
kangyoon115 <72716931+kangyoon115@users.noreply.github.com>
2024-07-08 22:32:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,24 +1,16 @@
-//while(answer==1)
-//if(num%2==0) n/2
-// else if(num%2==1) (n*3)+1
 class Solution {
-    public int solution(int num) {
-        int count=0;
-        if(num==1){
-            return 0;
-        }
-        while(num!=1){
-            if(num%2==0){
-                num/=2;
-            }else if(num%2==1){
-                num=num*3+1;
+    public int solution(long num) {
+        int answer = 0;
+        while(num>1){
+            num = (num%2==0)?num/2:3*num+1;
+            answer++;
+            if(answer==500){
+                answer = -1;
+                break;
             }
-            count++;
             
-            if(count>=500){
-                return -1;
-            }
         }
-        return count;
-}
+        
+        return answer;
+    }
 }
