f2143d9605c07aa4a5c22ace018ce3bb5f91dbfb
kangyoon115 <72716931+kangyoon115@users.noreply.github.com>
2025-03-07 11:50:12 +0900

    [level 0] Title: 정수 부분, Time: 0.03 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181850. 정수 부분/정수 부분.java
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -1,7 +1,5 @@
 class Solution {
-    public int solution(double flo) {
-        int answer=(int)flo/1;
-        
-        return answer;
+    public int solution(double flo) { 
+        return (int)flo;
     }
 }
