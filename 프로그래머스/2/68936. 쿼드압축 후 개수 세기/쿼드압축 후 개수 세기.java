9af77ed7951f40dfa1d1b9f117cc44e0c1cbd2c2
kangyoon115 <72716931+kangyoon115@users.noreply.github.com>
2025-03-25 22:21:08 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 8.21 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -5,25 +5,27 @@ class Solution {
         quad(arr,0,0,arr.length);
         return answer;
     }
-    
+
     public boolean zip(int[][] arr, int x, int y, int size, int val) {
         // x, y: 현재 탐색 시작 좌표
         // size: 현재 탐색 영역의 크기
         // val: 비교할 기준 값
         for(int i = x; i < x + size; i++)
             for(int j = y; j < y + size; j++)
+                // 하나라도 기준 값과 다르면 false 반환
                 if(arr[i][j] != val)
                     return false;
         return true;
     }
-    
+
     // 쿼드 트리 압축
     public void quad(int[][] arr, int x, int y, int size){
         // 현재 영역이 모두 같은 값인지 확인
         if(zip(arr,x,y,size, arr[x][y])){
             if(arr[x][y] == 1)
                 answer[1]++;
-            else answer[0]++;
+            else 
+                answer[0]++;
             return ;
         }
         
@@ -33,5 +35,6 @@ class Solution {
         quad(arr,x+size/2,y, size/2);
         quad(arr,x+size/2,y + size/2, size/2);
     }
+
     
 }
