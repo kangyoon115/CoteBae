814393bd820f21bce2d26fe5b34e13ed27e8cb88
kangyoon115 <72716931+kangyoon115@users.noreply.github.com>
2025-03-25 22:19:11 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 21.23 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,27 +1,37 @@
 class Solution {
     static int[] answer;
+    public int[] solution(int[][] arr) {
+        answer = new int[2];
+        quad(arr,0,0,arr.length);
+        return answer;
+    }
+    
     public boolean zip(int[][] arr, int x, int y, int size, int val) {
+        // x, y: 현재 탐색 시작 좌표
+        // size: 현재 탐색 영역의 크기
+        // val: 비교할 기준 값
         for(int i = x; i < x + size; i++)
             for(int j = y; j < y + size; j++)
                 if(arr[i][j] != val)
                     return false;
         return true;
     }
+    
+    // 쿼드 트리 압축
     public void quad(int[][] arr, int x, int y, int size){
+        // 현재 영역이 모두 같은 값인지 확인
         if(zip(arr,x,y,size, arr[x][y])){
             if(arr[x][y] == 1)
                 answer[1]++;
             else answer[0]++;
             return ;
         }
+        
+        // 영역이 같지 않으면 4등분하여 재귀 호출
         quad(arr,x,y, size/2);
         quad(arr,x,y + size/2, size/2);
         quad(arr,x+size/2,y, size/2);
         quad(arr,x+size/2,y + size/2, size/2);
     }
-    public int[] solution(int[][] arr) {
-        answer = new int[2];
-        quad(arr,0,0,arr.length);
-        return answer;
-    }
+    
 }
